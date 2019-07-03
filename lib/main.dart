fad26c44df6d84e071886ec3e70e566ddf60045b
Isaac Graves <igraves@hbs.edu>
2019-07-02 22:09:15 -0400

    first

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 import './glob.dart' as glob;
 
 void main() async {
-  await buildWidgets();
+  buildWidgets();
   runApp(MyApp());
 }
 
@@ -21,10 +21,11 @@ class MyApp extends StatelessWidget {
           backgroundColor: Color(0xFF5C748A),
           appBar: AppBar(
             elevation: 0.0,
-            title: Text(
+            title: Center(
+                child: Text(
               'M A A V',
               style: glob.headStyle(0xFFEFEFEF),
-            ),
+            )),
             backgroundColor: Color(0xFF5C748A),
             bottom: PreferredSize(
                 child: Padding(
@@ -58,25 +59,43 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: buildWidgets(),
+    return Container(
+      width: glob.width(context, 1),
+      child: SingleChildScrollView(
+        child: Column(
+          children: buildWidgets(),
+        ),
       ),
     );
   }
 }
 
 List<Widget> buildWidgets() {
-  List<Widget> children = [];
+  List<Widget> children = [
+    Padding(padding:EdgeInsets.only(top: 20)),
+    createLink('https://flutter.dev'),
+    createLink('tel:2069131335')
+  ];
 
   var json = jsonDecode(glob.data);
-  for(var item in json){
-    for(var resource in item['data']){
-      children.add(
-        Text(resource['title'])
-      );
+  for (var item in json) {
+    children.add(Text(item['title'], style: glob.subHeadStyle(0xFFEFEFEF)));
+    for (var resource in item['data']) {
+      children.add(Text(resource['title']));
+      children.add(Text(resource['website']));
     }
   }
 
   return children;
+}
+
+Widget createLink(url){
+  url = Uri.encodeFull(url);
+
+  return GestureDetector(
+    child: Text(url, style: TextStyle(decoration: TextDecoration.underline, color: Colors.blue, fontSize: 30.0)),
+    onTap: () async {
+      glob.launchURL(url);
+    }
+  );
 }
